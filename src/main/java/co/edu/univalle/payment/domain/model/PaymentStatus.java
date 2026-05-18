d56651f25cafbbc960c90fd027848bea7cd2bdda
001Miguel15cn <miguel.arboleda@correounivalle.edu.co>
2026-05-18 18:57:03 -0500

    se modifico el paymentstatus para serealizar el enum

--- src/main/java/co/edu/univalle/payment/domain/model/PaymentStatus.java
+++ src/main/java/co/edu/univalle/payment/domain/model/PaymentStatus.java
@@ -1,8 +1,21 @@
 package co.edu.univalle.payment.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum PaymentStatus {
     PENDIENTE,
     EN_PROCESO,
     APROBADO,
-    FALLIDO
-}
+    FALLIDO;
+
+    @JsonValue
+    public String toJson() {
+        return this.name(); // → "APROBADO", no 2
+    }
+
+    @JsonCreator
+    public static PaymentStatus fromJson(String value) {
+        return PaymentStatus.valueOf(value);
+    }
+    }
